0f2aea9866ddf07718f3bc837d70e22e0fb6620c
Furkan Düzyol <duzyolfurkan@gmail.com>
2023-01-13 18:08:34 +0300

    Veritabanına ait başlangıç sınıfları oluşturuldu.

---
+++ EntityLayer/Concrate/About.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrate
+{
+    public class About
+    {
+        public int AboutID { get; set; }
+        public string AboutDatails1 { get; set;}
+        public string AboutDatails2 { get; set; }
+        public string AboutImage1 { get; set; }
+        public string AboutImage2 { get; set; }
+        public string AboutMapLocation { get; set; }
+        public bool AboutStatus { get; set; }
+
+
+    }
+}
