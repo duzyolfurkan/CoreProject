0213fa90f4d6c77e26775d4edd6ae967932cd6b5
Furkan Düzyol <duzyolfurkan@gmail.com>
2023-01-19 02:37:38 +0300

    Repository tanımları yapıldı

---
+++ BussinessLayer/Concrate/CategoryManager.cs
@@ -0,0 +1,38 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrate
+{
+    public class CategoryManager : ICategoryService
+    {
+        public void CategoryAdd(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CategoryDelete(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CategoryUpdate(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetListAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataAccessLayer/Repositories/BlogRepository.cs
@@ -0,0 +1,48 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrate;
+using EntityLayer.Concrate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class BlogRepository : IBlogDal
+    {
+        public void AddBlog(Blog blog)
+        {
+            using var db = new Context();
+            db.Blogs.Add(blog);
+            db.SaveChanges();
+        }
+
+        public void DeleteBlog(Blog blog)
+        {
+            using var db = new Context();
+            db.Blogs.Remove(blog);
+            db.SaveChanges();
+        }
+
+        public Blog GetById(int id)
+        {
+            using var db = new Context();
+            return db.Blogs.Find(id);
+        }
+
+        public List<Blog> ListAllBlog()
+        {
+            using var db = new Context();
+            return db.Blogs.ToList();
+        }
+
+        public void UpdateBlog(Blog blog)
+        {
+            using var db = new Context();
+            db.Blogs.Update(blog);
+            db.SaveChanges();
+        }
+    }
+}
