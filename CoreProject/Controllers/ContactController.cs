430c072c344966e38acd436903849d3ade685928
Furkan Düzyol <duzyolfurkan@gmail.com>
2023-01-31 00:36:56 +0300

    Contact sayfası düzenlendi

---
+++ CoreProject/Controllers/ContactController.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLayer.Concrate;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrate;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CoreProject.Controllers
+{
+    public class ContactController : Controller
+    {
+        ContactManager cm = new ContactManager(new EfContactRepository());
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Contact contact)
+        {
+            contact.ContactDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            contact.ContactStatus = true;
+            cm.ContactAdd(contact);
+            return RedirectToAction("Index", "Blog");
+        }
+    }
+}
