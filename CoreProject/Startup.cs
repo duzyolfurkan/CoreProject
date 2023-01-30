b5eefb7cae19ebdc8bdefddb7c9384606e4f9f21
Furkan Düzyol <duzyolfurkan@gmail.com>
2023-01-30 22:49:18 +0300

    Hata sayfası düzenlendi

--- CoreProject/Startup.cs
+++ CoreProject/Startup.cs
@@ -39,6 +39,10 @@ namespace CoreProject
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            //Statü kodlarýný kullanmamýzý saðlayan, belli bir sayfaya yönlendirilmesini saðlayan metoddur.
+            app.UseStatusCodePagesWithReExecute("/ErrorPage/Error1","?code={0}");
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
